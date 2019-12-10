1efc857ce76e613ae7c829b46bcfeb5a5d0d7756
Rallefar1103 <42294918+Rallefar1103@users.noreply.github.com>
2019-12-10 11:36:19 +0100

    Update info_energy_saving.c
    Opdateret til nu at indtage en user_choice struct som parameter for på den måde, at kunne afgøre hvorledes vi kigger på DK1 eller DK2

--- Larry/info_energy_saving.c
+++ Larry/info_energy_saving.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NMB_OF_ELEMENTS 4
 #define KWH_TO_MWH 0.001
@@ -61,7 +62,17 @@ typedef struct {
     double price_area2;
 } priser;
 
+typedef struct{
+    int id;
+    char residence[5];
+    char language[5];
+    int choice_of_function;
+    int user_type;
+} user;
+
+
 double info_energy_saving(meterdata forbrug_array[], priser priser_array[], int time_frame);
+double info_energy_saving2(meterdata forbrug_array[], priser priser_array[], int time_frame, user user_choice);
 priser *cheapest(priser priser_array[], int time_frame);
 int cmpfunc(const void * a, const void * b);
 
@@ -69,7 +80,9 @@ int cmpfunc(const void * a, const void * b);
 int main(void)
 {
     double savings;
+    double savings2;
     priser cheapest_struct;
+    user user_choice = {1200, "DK1", "Dansk", 2, 1};
 
     priser priser_array[4] = {
         {
@@ -104,8 +117,11 @@ int main(void)
     printf("\n");
     /* printf("Pris område1: %.1f Pris område2: %.1f\n", data.price_area1, data.price_area2);*/
 
-    savings = info_energy_saving(meterdata_array, priser_array, 0);
-    printf("Hvis du flytter dit forbrug, sparer du: %.1f DKK\n\n", savings);
+    /* savings = info_energy_saving(meterdata_array, priser_array, 0);
+    printf("Hvis du flytter dit forbrug, sparer du: %.1f DKK\n\n", savings);*/
+
+    savings2 = info_energy_saving2(meterdata_array, priser_array, 0, user_choice);
+    printf("Hvis du flytter dit forbrug, sparer du: %.1f DKK\n\n", savings2);
 
     cheapest_struct = *cheapest(priser_array, 0);
     printf("Dato: %d , Klokkeslæt: %d - %d , Pris Område1: %.f DKK\n", cheapest_struct.from.day,
@@ -116,40 +132,61 @@ int main(void)
 }
 
 /* Funktionen returnerer besparelsen forbrugeren kan opnå hvis vedkommende flytter sit forbrug til det billigste tidspunkt 
- * Funktionen tager udgangspunkt i et 24 element-langt array der indekserer alle timerne og brugeren skriver da blot tidspunktet 
- * vedkommende ønsker at få informationer fra */
-double info_energy_saving(meterdata forbrug_array[], priser priser_array[], int time_frame)
+ * Funktionen tager udgangspunkt i et 24 element-langt dat-array der samt et user_choice der afgører om det er DK1 eller DK2 vi kigger på */
+double info_energy_saving2(meterdata forbrug_array[], priser priser_array[], int time_frame, user user_choice)
 {
-    double current_consumption;
-    double current_price;
-    double user_price_current;
-    double user_price_after;
-    double cheapest_price;
+    double current_consumption = 0.0;
+    double current_price = 0.0;
+    double user_price_current = 0.0;
+    double user_price_after = 0.0;
+    double cheapest_price = 0.0;
+    
+    if (strcmp(user_choice.residence, "DK1") == 0)
+    {   
+        current_price = priser_array[time_frame].price_area1;
+        current_consumption = forbrug_array[time_frame].amount;
 
-    current_price = priser_array[time_frame].price_area1;
-    current_consumption = forbrug_array[time_frame].amount;
+        if (current_price < 0)
+            printf("Prisen er pt. negativ!\n");
 
-    if (current_price < 0)
-        printf("Prisen er pt. negativ!\n");
+        /* Dette giver brugerens nuværende strømpris baseret ud fra forbruget */
+        user_price_current = current_consumption * KWH_TO_MWH * current_price;
+        printf("Nuværende pris baseret på nuværende forbrug: %.1f DKK\n", user_price_current);
 
-    /* Dette giver brugerens nuværende strømpris baseret ud fra forbruget */
-    user_price_current = current_consumption * KWH_TO_MWH * current_price;
-    printf("Nuværende pris baseret på nuværende forbrug: %.1f DKK\n", user_price_current);
+        /* Sorterer pris-array så den billigste pris ligger først */
+        qsort(priser_array, NMB_OF_ELEMENTS, sizeof(priser), cmpfunc);
+        cheapest_price = priser_array[0].price_area1;
 
-    /* Sorterer pris-array så den billigste pris ligger først */
-    qsort(priser_array, NMB_OF_ELEMENTS, sizeof(priser), cmpfunc);
-    cheapest_price = priser_array[0].price_area1;
+        /* Dette giver brugerens strømpris baseret ud fra hvornår det er billigst at bruge strøm */
+        user_price_after = current_consumption * KWH_TO_MWH * cheapest_price;
+        printf("Den billigste pris for forbrugeren baseret på nuværende forbrug: %.1f DKK\n", user_price_after);
+    }
+    else 
+    {
+        current_price = priser_array[time_frame].price_area2;
+        current_consumption = forbrug_array[time_frame].amount;
 
-    /* Dette giver brugerens strømpris baseret ud fra hvornår det er billigst at bruge strøm */
-    user_price_after = current_consumption * KWH_TO_MWH * cheapest_price;
-    printf("Den billigste pris for forbrugeren baseret på nuværende forbrug: %.1f DKK\n", user_price_after);
+        if (current_price < 0)
+            printf("Prisen er pt. negativ!\n");
+
+        /* Dette giver brugerens nuværende strømpris baseret ud fra forbruget */
+        user_price_current = current_consumption * KWH_TO_MWH * current_price;
+        printf("Nuværende pris baseret på nuværende forbrug: %.1f DKK\n", user_price_current);
+
+        /* Sorterer pris-array så den billigste pris ligger først */
+        qsort(priser_array, NMB_OF_ELEMENTS, sizeof(priser), cmpfunc);
+        cheapest_price = priser_array[0].price_area2;
+
+        /* Dette giver brugerens strømpris baseret ud fra hvornår det er billigst at bruge strøm */
+        user_price_after = current_consumption * KWH_TO_MWH * cheapest_price;
+        printf("Den billigste pris for forbrugeren baseret på nuværende forbrug: %.1f DKK\n", user_price_after);
+    }
 
     if (user_price_after > user_price_current)
         return user_price_after - user_price_current;
     else
         return user_price_current - user_price_after;
 } 
-
 
 priser *cheapest(priser priser_array[], int time_frame)
 {
@@ -163,7 +200,7 @@ priser *cheapest(priser priser_array[], int time_frame)
     cheapest->price_area1 = priser_array[0].price_area1;
     cheapest->price_area2 = priser_array[0].price_area2;
 
-    /* Informerer forbrugren om hvorvidt det billigste tidspunkt er sent om aftenen */
+    /* Informerer forbrugeren om hvorvidt det billigste tidspunkt er sent om aftenen */
     if (priser_array[0].from.time.hour > 21)
         printf("Det billigste tidspunkt er efter kl 21 om aftenen\n");
 
@@ -179,5 +216,3 @@ int cmpfunc(const void * a, const void * b)
     return priserA->price_area1 - priserB->price_area1;
 }
 
-
- 
