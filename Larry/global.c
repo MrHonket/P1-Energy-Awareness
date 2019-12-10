fe4f34683a82946e0e126a3732aec4710e29b037
MartinBruun <mbm93fen@gmail.com>
2019-12-10 14:19:35 +0100

    global vars updated to correct format

--- Larry/global.c
+++ Larry/global.c
@@ -1,30 +1,13 @@
-/* Data structures */
-
+/*Universelle imports*/
 #include <stdio.h>
 #include <stdlib.h>
 
 /*Symbolske konstanter*/
-/*Overordnede*/
 #define FALSE       0
 #define TRUE        1
-/*Choices.usertype kan indeholde en af disse værdier*/
-#define HUMAN       0
-#define AUTOMATED   1
-/*Choices.choice kan indeholde en af disse værdier*/
-#define ERROR_TEST            0
-#define USER_HISTORY          1
-#define INFO_ENERGY_SAVING    2
-#define UPDATE_SETTINGS       3
-#define SYSTEM_INFORMATION    4
-#define WARNING_ENERGY_SAVING 5
-#define MACHINE_ACTIVATION    6
-#define CONSUMPTION_CHECK     7
-#define FUTURE_DATA           8
-/*Konstanter der beskriver en fejlmeddelse i error_message*/
-#define ERROR_CONFIRMATION_PASSIVE_MODULE 10
-#define ERROR_CHOICE_DOESNT_EXIST         11
-#define ERROR_INFO_STR_NOT_FOUND          12
-#define ERROR_USER_TYPE                   13
+#define SUCCESS     2
+
+/*DATA VARIABLE*/
 
 /* TID */
 typedef enum {
@@ -38,18 +21,18 @@ typedef enum {
 } ugedag;
 
 typedef enum{
-   januar=1,
-   februar,
-   marts,
-   april,
-   maj,
-   juni,
-   juli,
-   august,
-   september,
-   oktober,
-   november,
-   december
+   Januar=1,
+   Februar,
+   Marts,
+   April,
+   Maj,
+   Juni,
+   Juli,
+   August,
+   September,
+   Oktober,
+   November,
+   December
 }month;
 
 typedef struct {
@@ -63,7 +46,6 @@ typedef struct{
    month month;
    int    year;
 }dato;
-
 
 /* Area */
 typedef enum {
@@ -79,7 +61,6 @@ const char *ugedag_txt[] = {
    "Fredag",
    "Lordag",
    "Sondag"
-   
 };
 
 const char *month_txt[] = {
@@ -97,7 +78,6 @@ const char *month_txt[] = {
    "november",
    "december"
 };
-
 
 /* METERDATA */
 /* Målepunkt id   ;Fra dato        ;Til dato        ;Mængde;Måleenhed;Kvalitet;Type;
@@ -119,14 +99,12 @@ typedef struct {
  *        ;Hours  ; SYS   ; SE1   ;SE2   ;SE3   ;SE4   ;FI    ;DK1   ;DK2   ;Oslo  ;Kr.sand;Bergen;Molde ;Tr.heim;Tromsø;EE    ;LV    ;LT
 01-01-2017;00 - 01; 191,05; 178,64;178,64;178,64;178,64;178,64;155,82;155,82;211,13;211,13 ;211,13;178,64;178,64 ;178,64;178,64;178,64;178,64 */
 
-
 typedef struct{
    dato from;
    dato to;
    double DK1price;
    double DK2price;
 }pricedata;
-
 
 /* DATA */
 typedef struct{
@@ -170,6 +148,30 @@ typedef struct{
 
 }production;
 
+typedef enum {
+    Human,
+    Automated
+}user_type;
+
+typedef enum {
+    ErrorTest,
+    UserHistory,
+    InfoEnergySaving,
+    UpdateSettings,
+    SystemInformation,
+    WarningEnergySaving,
+    MachineActivation,
+    ConsumptionCheck,
+    FutureData
+}choice_function;
+
+typedef enum{
+    ErrorConfirmationPassiveModule,
+    ErrorChoiceDoesntExist,
+    ErrorInfoStrNotFound,
+    ErrorUserType
+}error_types;
+
 typedef struct{
     int id;
     char residence[5];
@@ -190,22 +192,22 @@ int error_message(int error);
 int debug_print(user user_choice, data user_data, int run_or_not){
     if(run_or_not){
         printf("ID = %d ; Sprog = %s ; Bosted = %s\n",user_choice.id,user_choice.language,user_choice.residence);
-        printf("Valgte funktion = %s ; Brugeren er %s\n",translate(user_choice.choice_of_function),(user_choice.user_type == HUMAN ? "menneske" : "automatisk"));
+        printf("Valgte funktion = %s ; Brugeren er %s\n",translate(user_choice.choice_of_function),(user_choice.user_type == Human ? "menneske" : "automatisk"));
     }
     return EXIT_SUCCESS;
 }
 
 char* translate(int choice_of_function){
     switch(choice_of_function){
-        case ERROR_TEST             : return "error_test";
-        case USER_HISTORY           : return "user_history";
-        case INFO_ENERGY_SAVING     : return "info_energy_saving";
-        case UPDATE_SETTINGS        : return "update_settings";
-        case SYSTEM_INFORMATION     : return "system_information";
-        case WARNING_ENERGY_SAVING  : return "warning_energy_savings";
-        case MACHINE_ACTIVATION     : return "machine_activation";
-        case CONSUMPTION_CHECK      : return "consumption_check";
-        case FUTURE_DATA            : return "future_data";
+        case ErrorTest           : return "error_test";
+        case UserHistory         : return "user_history";
+        case InfoEnergySaving    : return "info_energy_saving";
+        case UpdateSettings      : return "update_settings";
+        case SystemInformation   : return "system_information";
+        case WarningEnergySaving : return "warning_energy_savings";
+        case MachineActivation   : return "machine_activation";
+        case ConsumptionCheck    : return "consumption_check";
+        case FutureData          : return "future_data";
     }
     return "";
 }
--- Larry/global.h
+++ Larry/global.h
@@ -6,31 +6,12 @@
 #include <stdlib.h>
 
 /*Symbolske konstanter*/
-/*Overordnede*/
 #define FALSE       0
 #define TRUE        1
-/*Disse herunder kan evt. laves om til enums*/
-/*Choices.usertype kan indeholde en af disse værdier*/
-#define HUMAN       0
-#define AUTOMATED   1
-/*Choices.choice kan indeholde en af disse værdier*/
-#define ERROR_TEST            0
-#define USER_HISTORY          1
-#define INFO_ENERGY_SAVING    2
-#define UPDATE_SETTINGS       3
-#define SYSTEM_INFORMATION    4
-#define WARNING_ENERGY_SAVING 5
-#define MACHINE_ACTIVATION    6
-#define CONSUMPTION_CHECK     7
-#define FUTURE_DATA           8
-/*Konstanter der beskriver en fejlmeddelse i error_message*/
-#define ERROR_CONFIRMATION_PASSIVE_MODULE 10
-#define ERROR_CHOICE_DOESNT_EXIST         11
-#define ERROR_INFO_STR_NOT_FOUND          12
-#define ERROR_USER_TYPE                   13
+#define SUCCESS     2
 
 /*VARIABLE TIL data*/
-    /* tids enums og structs: ugedag, month, time, dato, ugedag_txt[],month_txt[]. */
+    /* tids enums og structs: ugedag, month, time, dato, ugedag_txt[],month_txt[].*/
     typedef enum {Man,Tir,Ons,Tor,Fre,Lor,Son} ugedag;
     typedef enum{Januar=1,Februar,Marts,April,
         Maj,Juni,Juli,August,September,
@@ -59,27 +40,29 @@ const char *ugedag_txt;
 const char *month_txt;
 
 /*VARIABLE TIL user*/
-    /*IMPLEMENTERES HVIS VI ØNSKER DET!!!*/
+    /*symbolske konstanter for brugertypen og valg af funktion*/
+    typedef enum {Human,Automated}user_type;
+    typedef enum {ErrorTest,UserHistory,InfoEnergySaving,UpdateSettings,
+              SystemInformation,WarningEnergySaving,MachineActivation,
+              ConsumptionCheck,FutureData}choice_function;
     /*settings valg: settings*/
-    typedef struct{
-        //Hvis user skal udvides med en choice og en settings struct for at abstrahere mere.
-    }settings;
+    typedef struct{int id;char residence[5];char language[5]}settings;
     /*aktive valg: choice*/
-    typedef struct{
-        //Hvis user skal udvides med en choice og en settings struct for at abstrahere mere.
-    }choice;
+    typedef struct{int function}choice;
 /*user structet*/
 typedef struct{
-    int id;
-    char residence[5];
-    char language[5];
-    int choice_of_function;
-    int user_type;
+    settings settings;
+    choice choice;
+    user_type type;
 }user;
 
+/*De forskellige fejltyper. Bruges med error_message*/
+typedef enum{ErrorConfirmationPassiveModule,ErrorChoiceDoesntExist,
+             ErrorInfoStrNotFound,ErrorUserType}error_types;
 /*debugging Prototypes*/
 int debug_print(user user_choice, data user_data,int run_or_not);
 char* translate(int choice_of_function);
 int error_message(int error);
 
 #endif //GLOBAL_H
+
