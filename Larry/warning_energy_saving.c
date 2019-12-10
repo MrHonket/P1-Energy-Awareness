5b85cab6feea334045cd4d822cc0d483ddeff4bb
MathiasNielsen1206 <56735904+MathiasNielsen1206@users.noreply.github.com>
2019-12-10 14:32:03 +0100

    Update warning_energy_saving.c

--- Larry/warning_energy_saving.c
+++ Larry/warning_energy_saving.c
@@ -4,46 +4,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "global.h"
-/* #include "database_module.h" */
+#include "database_module.h"
 
-double warning_energy_saving(double price, double consumption,double median_consumption);
+double warning_consumption(double price, double consumption,double median_consumption);
 int main(void){
   double price_treshold;
   double median_consumption=0.5;
   printf("hello and welcome to the gokkeslaet\n");
   scanf("%lf %lf",&price_treshold, &median_consumption);
-  pricedata data= {{20,0,15,januar,2018},{21,0,15,januar,2018},219.3,222.4};
-  meterdata consumption= {"1234",{20,0,15,januar,2018},{21,0,15,januar,2018},3.3,"kwh","measured","hour"}; 
-  printf("gokkeslaet: %d\n",data.from.time.hour);   
-  printf("slut gokker: %d\n",data.to.time.hour);
-  printf("dato: %d. %s %d\n",data.from.day,month_txt[data.from.month],data.from.year); 
+  pricedata data= {{20,0,15,Januar,2018},{21,0,15,Januar,2018},219.3,222.4};
+  meterdata consumption= {"1234",{20,0,15,Januar,2018},{21,0,15,Januar,2018},3.3,"kwh","measured","hour"}; 
   printf("priser: %lf, %lf\n",data.DK1price,data.DK2price);
-  printf("Forbrug i perioden: %lf\n",consumption.value);
-  if(data.DK1price<=price_treshold)
+  if(data.DK1price>=price_treshold)
   {
-    printf("you are paying %.2lf percent more than usual",warning_energy_saving(data.DK1price,consumption.value,median_consumption));
+    printf("your consumption is %.2lf percent off the usual\n",warning_consumption(data.DK1price,consumption.value,median_consumption));
   }
-  else if (data.DK2price<=price_treshold)
+  else if (data.DK2price>=price_treshold)
   {
-    printf("you are paying %.2lf percent more than usual",warning_energy_saving(data.DK2price,consumption.value,median_consumption));
+    printf("your consumption is %.2lf percent off the usual\n",warning_consumption(data.DK2price,consumption.value,median_consumption));
   }
-
-printf("Forbrug i perioden: %lf\n",consumption.value);
-if(data.DK1price<=price_treshold|| data.DK2price<=price_treshold){
-printf("the price difference is: %.2lf",warning_energy_saving(data.DK2price,consumption.value,median_consumption));
-}
-else{
+else
+{
 printf("Price is lower than threshold no worries\n");
 }
+printf("Forbrug i perioden: %lf\n",consumption.value);
     return EXIT_SUCCESS;
 }
-double warning_energy_saving(double price,double consumption,double median_consumption){
+double warning_consumption(double price,double consumption,double median_consumption){
 double price_high, price_difference;
 if(consumption>median_consumption){
-price_difference=(price*consumption)-(price*median_consumption);
+  price_difference=(price*consumption)/(price*median_consumption);
 printf("your energy consumption is higher than normally right now.\n");
 return price_difference;
 }
-printf("Price Difference %.2lf",price_difference);
+else if(consumption<=median_consumption){
+  price_difference=(price*median_consumption)/(price*consumption);
+printf("your consumption is not higher than normal\n");
 return price_difference;
+}
 }
