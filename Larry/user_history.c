909f00f2961c4c9a7259819a113dbfc35d282fdc
basilbees <basw0724@gmail.com>
2019-12-11 10:05:09 +0100

    Update user_history.c
    Nu kan den udregne gennemsnit og median

--- Larry/user_history.c
+++ Larry/user_history.c
@@ -1,73 +1,69 @@
 
-/***
- *      ____               _  _  _              _                                    _         _       
- *     |  _ \             (_)| |( )            (_)                                  | |       | |      
- *     | |_) |  __ _  ___  _ | ||/ ___   _ __   _   ___  ___   _ __ ___    ___    __| | _   _ | |  ___ 
- *     |  _ <  / _` |/ __|| || |  / __| | '_ \ | | / __|/ _ \ | '_ ` _ \  / _ \  / _` || | | || | / _ \
- *     | |_) || (_| |\__ \| || |  \__ \ | | | || || (__|  __/ | | | | | || (_) || (_| || |_| || ||  __/
- *     |____/  \__,_||___/|_||_|  |___/ |_| |_||_| \___|\___| |_| |_| |_| \___/  \__,_| \__,_||_| \___|
- *                                                                                                     
- *                                                                                                     
- */
-
-/*
+/*                                                                                                          *
+ *      ____               _  _  _                 _                                       _         _      * 
+ *     |  _ \             (_)| |( )               (_)                                     | |       | |     * 
+ *     | |_) |  __ _  ___  _ | ||/ ___      _ __   _   ___  ___      _ __ ___    ___    __| | _   _ | |     *
+ *     |  _ <  / _` |/ __|| || |  / __|    | '_ \ | | / __|/ _ \    | '_ ` _ \  / _ \  / _` || | | || |     *
+ *     | |_) || (_| |\__ \| || |  \__ \    | | | || || (__|  __/    | | | | | || (_) || (_| || |_| || |     *
+ *     |____/  \__,_||___/|_||_|  |___/    |_| |_||_| \___|\___|    |_| |_| |_| \___/  \__,_| \__,_||_|     *
+ *                                                                                                          *
+ *                                                                                                          *
 user_history
-- Inputparameter er user_choice og user_data.
+- Inputparameter er user_choice (er det en median eller gennemsnit, der ønskes) og user_data (ideelt en array af værdier) og antal
+  elementer i arrayet.
 - return-værdien er en double (median-return bliver lavet om til en int), enten gennemsnit eller median, baseret på user_choice
 - gennemsnit udregnes ved at lægge alle værdierne sammen fra user_data og dividere med antal elementer i arrayet
 - median udregnes ved først at sortere alle værdierne fra user_data, dernæst dividere antal elementer med 2, 
   hvilket giver 'i' og så tage værdien af det i'ende element i user_data.
-- skal evt. hedde noget andet end user_history, eftersom den jo egentlig bare returnerer en double
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "global.h"
 
-double user_history(int mean_or_median, double dataset[]);
+double user_history(int mean_or_median, double dataset[], int number_of_elements);
+int cmpfunc (const void * a, const void * b);
 
 int main(void) {
-    double data[] = {1.5, 9.2, 3.4, 5.6, 2.3}; /*Midlertidig data-array ;)*/
-    int choice = 0, cont = 0;
-    
-    do {
-        printf("For mean, press 1, for median, press 2: ");
-        scanf("%d", &choice);
+    /*Mock-data)*/
+    double data[] = {1.5, 9.2, 3.4, 5.6, 2.3};
+    int choice1 = 1, choice2 = 2, n = 5;
 
-        printf("%lf\n", user_history(choice, data)); /*choice angiver om der ønskes gennemsnit eller median
-                                                     data[] er et array af structs (enten pris, forbrug eller grøn andel)*/
-        
-        printf("Press 1 to run again, press q to quit.");
-        scanf("%d\n", &cont);
-
-        if (cont != 1) {return 0;}
-
-    } while (cont == 1);
+        printf("Mean: %lf\nMedian: %lf", user_history(choice1, data, n), user_history(choice2, data, n)); 
+        /*choice angiver om der ønskes gennemsnit eller median 
+          data[] er et array af structs (enten pris, forbrug eller grøn andel)*/
 
     return 0;
 }
 
-double user_history(int mean_or_median, double dataset[]) {
-    int i = 0, mean = 0, median = 0;
+double user_history(int mean_or_median, double dataset[], int number_of_elements) {
+    int i = 0;
+    double mean = 0;
 
     if (mean_or_median == 1) {
         /*Udregner gennemsnit*/
-        for (i = 0; i < sizeof(dataset); i++) {
+        for (i = 0; i < number_of_elements; i++) {
             mean += dataset[i];
         }
         /*Returnerer gennemsnittet*/
         printf("%lf, %d\n", mean, i);
-        return mean/i;
+        return (mean/i);
 
     } else
     if (mean_or_median == 2) {
         /*Udregner median*/
-        /*Sorterer arrayet - qsort?*/
-        /*Tæl antal elementer, evt. samtidig*/
-        return dataset[antal_elementer/2];
+        qsort(dataset, number_of_elements, sizeof(double), cmpfunc);
+
+        /*Returnerer median*/
+        return dataset[number_of_elements/2];
     } else
     {
         /*Error*/
         return 0;
     }
 
+}
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(double*)a - *(double*)b );
 }
