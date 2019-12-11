bea47b9a0a6421213f827483c9651738e1c1e9b7
saxjax <dev@saxjax.dk>
2019-12-11 23:19:03 +0100

    database to doooo's

--- Larry/database_module.h
+++ Larry/database_module.h
@@ -1,11 +1,21 @@
 /* Jakob */
 #ifndef DATABASE_MODULE_H
 #define DATABASE_MODULE_H
+#include "global.h"
 
 void init_database(void);
 data *get_price_for_timeinterval_in_area(dato from, dato to,  area area);
-data *get_consumption_for_timeinterval_at_id(dato from, dato to, char *id);
-dato date_from_string(char *date, int time);
+
+/* HELPER functions */
+/* 01-01-2017  22 */
+dato    date_from_stringDMYI(char *str,int time);
+/* 2017 01 01 22.00 */
+dato    date_from_stringYMDH(char *date);
+
+void    print_price_index(int index);
+void    print_consump_index(int index);
+
+
 
 
 #endif //DATABASE_MODULE_H
