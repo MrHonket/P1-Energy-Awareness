be6139aa2e4b96151357be3e15183a54455d2022
Rallefar1103 <42294918+Rallefar1103@users.noreply.github.com>
2019-12-12 17:03:32 +0100

    space

--- Larry/active_module_rallefar.c
+++ Larry/active_module_rallefar.c
@@ -59,7 +59,7 @@ int main(void){
     for (int i = 0; i < 24; i++)
         printf("Dato: %d\t| Tidspunkt: %d - %d\t| Priser: %.2f\t| Forbrug: %.2f\n", Data[i].prize.from.day, Data[i].prize.from.time.hour, 
                                                                  Data[i].prize.to.time.hour, Data[i].prize.DK1price, Data[i].meter.value);
-    // printf("\n\n\n\nTidspunkt vi kigger efter: %d\n\n\n\n", User.choice.from.time.hour); //
+    printf("\n\n\n\nTidspunkt vi kigger efter: %d\n\n\n\n", User.choice.from.time.hour); 
 
     if (User.type == Human){
         prompt_user(User, Data, cheapest_struct);
