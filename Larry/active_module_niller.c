2b299a1906c2a7022b09738320ebc1990a36f465
MathiasNielsen1206 <56735904+MathiasNielsen1206@users.noreply.github.com>
2019-12-12 10:38:59 +0100

    kører sammen med active module så er der tests tilbage
    mangler stadig median værdier fra user history

---
+++ Larry/active_module_niller.c
@@ -0,0 +1,150 @@
+/*MartinBM*/
+//"Rigtige" gcc compilation
+//gcc active_module.c global.c language.c database_module.c passive_module.c update_settings.c info_energy_saving.c user_history.c warning_energy_saving.c system_information.c machine_activation.c future_data.c consumption_check.c debug.c
+//"Experimental" gcc compilation
+//gcc active_module.c global.c language.c database_module.c user_history_exp.c update_settings.c info_energy_saving_exp.c passive_module.c warning_energy_saving_exp.c system_information.c machine_activation.c future_data.c consumption_check.c debug.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "global.h"                 //Implementeret og brugbart!
+#include "language.h"               //tom .h OG .c fil
+
+#include "database_module.h"        //Implementeret og brugbart men giver forskellige fejlmeddelser.
+#include "user_history_exp.h"       //ERROR SYNTAX! IMPLEMENTERET SOM EXP!
+#include "update_settings.h"        //Implementeret, men load_data loader ikke data korrekt. MockData skabt i main() for at omgå.
+#include "info_energy_saving_exp.h" //IMPLEMENTERET SOM EXP! Der sker fejl ved linje 51
+#include "passive_module.h"         //Implementeret og brugbart
+#include "warning_energy_saving_exp.h"//IMPLEMENTERET SOM EXP! Der bruges dog pt. MockData i funktionen for at kalde underfunktioner.
+
+#include "system_information.h"     //Implemented som error_message!
+#include "machine_activation.h"     //Implemented som error_message!
+#include "future_data.h"            //Implemented som error_message!
+#include "consumption_check.h"      //Implemented som error_message!
+#include "debug.h"                  //implementeret og brugbart!
+
+/*Dette er prototyper i programmet.*/
+void check_activation(user User);
+int prompt_user(user User,data *Data);
+void log_data(user User);
+/*main vil modtage information om det er en måler eller sig selv (Automatisk) der aktivere eller en app (Human)*/
+int main(void){
+    user User;                           
+    data *Data;
+    int confirmation;
+    
+    User.settings = load_settings();
+    check_activation(User);
+    
+    // MockData!
+    User.type = Automated;
+    User.choice.function = WarningEnergySaving;
+    dato dato1 = {{18, 00}, 4, Januar, 2018};
+    dato dato2 = {{19, 00}, 4, Januar, 2018};
+    User.choice.lookup = Price;
+    User.choice.mean_or_median = Median;
+    User.settings.id = 200;
+    strcpy(User.settings.language,"DK");
+    strcpy(User.settings.residence,"DK1");
+    // End mock 
+
+    
+
+    Data = get_price_for_timeinterval_in_area(dato1,dato2, Dk1);
+
+
+    if (User.type == Human){
+        prompt_user(User,Data);
+    }
+    else if(User.type == Automated){
+        confirmation = passive_module(User,Data);
+        if(confirmation){
+            log_data(User);
+        }
+        else{
+            error_message(ErrorConfirmationPassiveModule);
+        }
+    }
+    else{
+        error_message(ErrorUserType);
+    }
+
+    debug_user(User);
+    
+    return EXIT_SUCCESS;
+}
+/*Checker for om der skal aktiveresSætter næste automatiske aktivering
+ *Hvis aktivatitionen sker automatisk skal den lave en udregning. 
+ *Ellers hvis aktivationen sker af et menneske skal den bare gå videre.
+ *Hvis den er automatisk og vurderes til at aktivere nu skal den hurtigt udregne næste gang den vurdere at aktivere hele programmet.
+ *Hvis den er automatisk men vurderes til ikke at aktivere nu, så skal den returnere main(Automated) efter X antal tid(1 time?)*/
+void check_activation(user User){
+    
+}
+/*Funktion der logger brugen af programmet og de data der måtte komme derigennem.*/
+void log_data(user User){
+
+}
+/*Funktionen som fungere som en brugers interface*/
+int prompt_user(user User, data *Data){
+    char info_str[60] = {'T','E','S','T'};
+    double info;
+    int new_command = 0;
+    
+    /*Basil was here & coded user interaction*/
+    printf("Tryk %d for brugerhistorik\n",UserHistory);
+    printf("Tryk %d for brugerindstillinger\n",UpdateSettings);
+    printf("Tryk %d for info om dine energibesparelser\n",InfoEnergySaving);
+    printf("Tryk %d for at lave et elcheck",ConsumptionCheck);
+    printf("Tryk %d for informationer om dette system\n",SystemInformation);
+    printf("Tryk %d for et gaet om fremtidige priser",FutureData);
+
+    scanf(" %d", &User.choice.function);
+    /*Ja, dette kunne godt være en switch :P*/
+    if(User.choice.function == UserHistory){
+        info = user_history(User, Data);
+    }
+    else if(User.choice.function == UpdateSettings){
+        update_settings();
+    }
+    else if(User.choice.function == InfoEnergySaving){
+        info = info_energy_saving(User, Data);
+        printf("Din besparelse er: %.5f DKK\n", info);
+    }
+    else if(User.choice.function == ConsumptionCheck){
+        info = consumption_check(User, Data);
+    }
+    else if(User.choice.function == SystemInformation){
+        info = system_information(User, Data);
+    }
+    else if(User.choice.function == FutureData){
+        future_data(User, Data);
+    }    
+    else if(User.choice.function == 0){
+        return 0;
+    }
+    else{
+        error_message(ErrorChoiceDoesntExist);
+    }
+
+    //Test af om info_str er blevet retuneret korrekt
+    if(info_str != NULL){
+        printf("%s\n",info_str);
+    }
+    else{
+        error_message(ErrorInfoStrNotFound);
+    }
+
+    //scanf for om der ønskes ny kommando. Basil was here
+    printf("Tryk paa enhver tast for at kore programmet igen.\nTryk 0 for at lukke programmet");
+    scanf("%d", &new_command);
+
+    if(new_command){
+        return prompt_user(User,Data);
+    }
+    else{
+        return 0;
+    }
+}
--- Larry/warning_energy_saving_exp.c
+++ Larry/warning_energy_saving_exp.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "global.h"
 #include "user_history_exp.h"
-//#include "database_module.h"
+#include "database_module.h"
 #define KWH 1000
 #define NMB_OF_ELEMENTS 24
 
@@ -15,22 +15,22 @@ double warning_consumption(double price, double consumption,double median_consum
    time data hvor får vi det fra?*/
 
 double warning_energy_saving(user User, data *Data){
+    dato to= {{24, 00}, 15, Januar, 2018};
     double value,
            one_price,          //Indtil videre tager funktionen en price værdi og ikke et strukt af hvad jeg kan se?
            one_consumption,    //Indtil videre tager funktionen en consumption værdi og ikke et strukt af hvad jeg kan se?
            median_consumption;
-    if(User.settings.residence==Dk1)
+    if(User.settings.residence=="DK1")
     {
-        one_price = Data->prize.DK1price;
+        one_price= Data[0].prize.DK1price;  
     }
-    else if(User.settings.residence==Dk2)
+    else if(User.settings.residence=="DK2")
     {
-        one_price = Data->prize.DK1price;
+        one_price =Data[0].prize.DK2price;
     }
     //Skal findes i Data.prize.DK1/2price men kan IKKE tilgås sådan grundet det er en pointer til et struct.
-    one_consumption = Data->meter.value; //Skal findes i Data.meter.unit el. value men kan IKKE tilgås sådan grundet det er en pointer til et struct.
-    User.choice.mean_or_median=Median;
-    median_consumption = user_history(User,Data);
+    one_consumption = Data[0].meter.value; //Skal findes i Data.meter.unit el. value men kan IKKE tilgås sådan grundet det er en pointer til et struct.
+    median_consumption = 0.3;/*  user_history(User,Data); */
        //= user_history hvor User.choice.mean_or_median = Median før User sendes i funktionen.
     /*Endmock*/
 
@@ -41,7 +41,6 @@ double warning_energy_saving(user User, data *Data){
 
 double warning_consumption(double price,double consumption,double median_consumption){
     double price_difference;
-    consumption/=KWH;
     if(consumption>median_consumption){
     price_difference=((price*consumption)/(price*median_consumption)-1);
     printf("your energy consumption is higher than normally right now.\n");
