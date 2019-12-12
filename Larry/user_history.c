dcb0f86a4372304cf56cf129fcf3880d8e39737f
basilbees <basw0724@gmail.com>
2019-12-12 09:32:57 +0100

    Update user_history.c

--- Larry/user_history.c
+++ Larry/user_history.c
@@ -22,38 +22,38 @@ user_history
 #include <stdlib.h>
 #include "global.h"
 #include "database_module.h"
-#include "calctime.h"
+#include  "calc_time.h"
 
 double calc_mean(double dataset[], int number_of_elements);
 double calc_median(double dataset[], int number_of_elements);
-int cmpfunc(const void *a, const void *b);
+int sort_array_for_median(const void *a, const void *b);
 
 /*Denne main skal hedde user_history og have inputparametre User og Data (indeholder en value-array)*/
-int main(void) {
-    user User; /*Indeholder indstillinger og brugervalg, User.choice*/
-    data *Data; /*Indeholder data for både meter-data og elpris*/
+int user_history(user User, data *Data) {
     int i = 0;
 
-    /*Lav en array ud fra data og baseret på, om dataen er fra meter eller pris. Det er denne, der skal regnes på.
-      Funktioner fra datamodulet*/
+    /*Lav en array ud fra Data-structen og baseret på, om dataen er fra meter eller pris. Det er denne, der skal regnes på.*/
     /*User.choice.meter & price skal lige defineres i global.c & global.h*/
-    if (User.choice.lookup_type == Meter) {
+    if (User.choice.lookup == Meter) {
         /*Initialiserer number_of_elements, samt den array, der skal bruges. Lav om til egen funktion.*/
-        int number_of_elements = calctime(Data.meterdata.to, Data.meterdata.from);/*Finder antal timer*/
+        int number_of_elements = calc_time(Data.meterdata.to, Data.meterdata.from);/*Finder antal timer*/
         double dataset[number_of_elements];
         /*Assigner arrayelementer fra Data og en for-løkke*/
         for (i = 0; i <= number_of_elements; i++) {
-            dataset[i] = Data->meterdata->value[i];
+            dataset[i] = Data[i]->meterdata->value;
         }
     } else
-    if (User.choice.lookup_type == Price) {
+    if (User.choice.lookup == Price) {
         /*Initialiserer number_of_elements, samt den array, der skal bruges. Lav om til egen funktion.*/
-        int number_of_elements = calctime(Data.pricedata.to, Data.pricedata.from);/*Finder antal timer*/
+        int number_of_elements = calc_time(Data.pricedata.to, Data.pricedata.from);/*Finder antal timer*/
         double dataset[number_of_elements];
         /*Assigner arrayelementer vha Data og en for-løkke*/
         for (i = 0; i <= number_of_elements; i++) {
-            dataset[i] = Data->pricedata->value[i];
+            dataset[i] = Data[i]->pricedata->value;
         }
+    } else {
+        /*Error*/
+        //error_message()
     }
 
     if (User.choice.mean_or_median == mean) {
@@ -61,6 +61,8 @@ int main(void) {
     } else
     if (User.choice.mean_or_median == median) {
         return calc_median(dataset, number_of_elements);
+    } else {
+        /*Error*/
     }
 }
 
@@ -80,13 +82,13 @@ double calc_mean(double dataset[], int number_of_elements) {
 
 double calc_median(double dataset[], int number_of_elements) {
     /*Udregner median*/
-    qsort(dataset, number_of_elements, sizeof(double), cmpfunc);
+    qsort(dataset, number_of_elements, sizeof(double), sort_array_for_median);
 
     /*Returnerer median*/
     return dataset[number_of_elements/2];
 }
 
 /*qsort*/
-int cmpfunc(const void *a, const void *b) {
+int sort_array_for_median(const void *a, const void *b) {
    return (*(double*)a - *(double*)b);
 }
