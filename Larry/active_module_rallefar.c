7fdfed7c78d3d1f4a869a9856cd032fcf567cca2
Rallefar1103 <42294918+Rallefar1103@users.noreply.github.com>
2019-12-12 16:48:20 +0100

    hh

--- Larry/active_module_rallefar.c
+++ Larry/active_module_rallefar.c
@@ -56,7 +56,7 @@ int main(void){
     End mock */
 
     Data = get_price_for_timeinterval_in_area(from, to, Dk1);
-    printf("Tidspunkt vi kigger efter: %d\n", User.choice.from.time.hour);
+    printf("\n\n\n\nTidspunkt vi kigger efter: %d\n\n\n\n", User.choice.from.time.hour);
     for (int i = 0; i < 24; i++)
         printf("Dato: %d\t| Tidspunkt: %d - %d\t| Priser: %.2f\t| Forbrug: %.2f\n", Data[i].prize.from.day, Data[i].prize.from.time.hour, 
                                                                  Data[i].prize.to.time.hour, Data[i].prize.DK1price, Data[i].meter.value);
