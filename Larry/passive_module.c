c5fa8de1b07c8b8e42999de387fd3804ca3e278f
MathiasNielsen1206 <56735904+MathiasNielsen1206@users.noreply.github.com>
2019-12-12 11:32:00 +0100

    alle mulige fede ting i passive modulet
    ikke?

--- Larry/passive_module.c
+++ Larry/passive_module.c
@@ -3,13 +3,17 @@
 #include <stdlib.h>
 #include "global.h"
 #include "warning_energy_saving_exp.h" //IMPLEMENTERET SOM EXP!!!!
-#include "machine_activation.h"
-
+#include "machine_activation.h"  
+#include "update_settings.h"
 int passive_module(user User, data *Data);
 
 /* returns 1 for exit_succes and 0 for exit_failure */
 int passive_module(user User, data *Data)
 {
+   int run_module; 
+   run_module = check_for_run_module(User, Data);
+   update_next_activation();
+
    if(User.choice.function == WarningEnergySaving)
       {
       printf("IMPLEMENTERET SOM EXP VERSION I PASSIVE_MODULE!!!\n");
@@ -25,5 +29,5 @@ int passive_module(user User, data *Data)
    else
       return 0;
 
+   update_next_activation();
 }
-
